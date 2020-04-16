45c94292d56d844e28c18730a91ec6c6f4c93d4d
Harikrishna Palla <HarikrishnaPa@hcl.com>
2020-04-16 12:05:42 +0530

    Jenkins test

---
+++ src/test/java/com/apple/test/TestClient.java
@@ -0,0 +1,19 @@
+/*
+package com.apple.test;
+
+import com.apple.demo.DemoApplication;
+import com.apple.demo.Products;
+import org.junit.Assert;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class TestClient {
+ @Test
+    public void TestCaseOne(){
+        assertEquals(11,11);
+    }
+
+
+}
+*/
